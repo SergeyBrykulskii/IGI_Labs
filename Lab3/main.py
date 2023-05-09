bdf85f3a4052a4697b1ba378530d9f3c185f311f
Sergey Brykulskii <serxio6758sk@gmail.com>
2023-05-09 18:20:13 +0300

    удалил нерабочую функцию

--- Lab3/main.py
+++ Lab3/main.py
@@ -16,20 +16,28 @@ def sm(a, b):
 
 # print(s[:-1])
 
-class A:
-    self._init_
+class Person:
+    religion = "aboba"
+
+    def __init__(self, age=3):
+        self.age = age
+
+    def get_age(self):
+        return self.age
 
 os.makedirs(os.path.dirname(file_path_json), exist_ok=True)
 with open(file_path_json, 'w') as output_file:
-    JsonParser.dump(sm, output_file)
+    JsonParser.dump(Person, output_file)
 
 with open(file_path_json, 'r') as input_file:
     a = JsonParser.load(input_file)
-    print(sm(1, 3))
+    per = Person()
+    p = a()
+    print(p.get_age() == per.get_age())
 os.makedirs(os.path.dirname(file_path_xml), exist_ok=True)
 with open(file_path_xml, 'w') as output_file:
     XmlParser.dump(sm, output_file)
 
 with open(file_path_xml, 'r') as input_file:
     a = XmlParser.load(input_file)
-    print(a(1, 4))
+    print(sm(2, 4))
--- Lab3/parsers/xml/xml_functions.py
+++ Lab3/parsers/xml/xml_functions.py
@@ -14,50 +14,6 @@ def serialize_xml(obj) -> str:
     else:
         return obj
 
-
-# def deserialize_xml(obj: str):
-#     # <tuple><str>type</str><str>int</str></tuple><tuple><str>value</str><int>1</int></tuple>
-#     # <tuple><tuple><str><str>type</str><str>int</str></str></tuple><tuple><int><str>value</str><int>5</int></int></tuple></tuple>
-#     # (('type','int'),('value','4'))
-#     # [["type", "int"], ["value", "4"]]
-#     if len(obj) == 0:
-#         return tuple()
-#     if obj[0] == '<':
-#         ind = obj.find('>')
-#         tag = obj[1:ind]
-#         obj = obj[ind+1:len(obj)]
-#         deserialized_obj = []
-#         depth = 1
-#         substr = ""
-#         i = 0
-#         while i < len(obj):
-
-#             if obj[i] == '<' and obj[i+1:i+len(tag)+1] == tag:
-#                 if depth == 0:
-#                     obj = obj[ind+1:len(obj)]
-#                     i = 0
-#                 depth += 1
-#             elif obj[i] == '/' and obj[i+1:i+len(tag)+1] == tag:
-#                 depth -= 1
-#             elif depth == 0:
-#                 deserialized_obj.append(
-#                     deserialize_xml(substr[0:len(substr)-2]))
-#                 i += len(tag) + 1
-#                 obj = obj[i:len(obj)]
-#                 i = 0
-#                 substr = ""
-#                 ind = obj.find('>')
-#                 tag = obj[1:ind]
-#                 continue
-
-#             substr += obj[i]
-#             i += 1
-
-#         deserialized_obj.append(deserialize_xml(substr))
-#         deserialized_obj.pop()
-#         return tuple(deserialized_obj)
-#     else:
-#         return obj
     
 
 def deserialize_xml(obj: str):
